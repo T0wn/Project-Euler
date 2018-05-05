b9138c3c3d4e789bf153dafd9bdd35ee2d120272
T0wn <eirijomine@hotmail.com>
2018-05-05 23:58:19 +0200

    problem 3 løst

--- problem3.py
+++ problem3.py
@@ -1,18 +1,11 @@
 
+
 def isFactor(tall, factor):
     if ( tall % factor == 0 ):
         return True
     return False
 
-def arrayPrimes(n):
-    sieve = [True] * n
-    for p in range(2, n):
-        if sieve[p]:
-            for i in range(p*p, n, p):
-                sieve[i] = False
-    return sieve
 
-'''
 def isPrime(tall):
     prime = True
 
@@ -25,13 +18,18 @@ def isPrime(tall):
             break
         
     return prime
-'''
 
-primes = arrayPrimes(600851475143)
-for i in range(0, 600851475143):
-    if (primes[i]):
-        print (i)
-    
+highestNr = 0
+n = 600851475143
+i = 3
+while ( i * i < n ):
+    if isFactor(n, i):
+        if isPrime(i):
+            if i > highestNr:
+                highestNr = i
+    i+=1
+
+print (highestNr)
 
 wait = input("PRESS ENTER TO CLOSE")
 exit()
