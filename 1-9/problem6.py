ca813d87a010bece1bd381088a1dcd51a93778a9
T0wn <eirijomine@hotmail.com>
2018-05-06 02:40:39 +0200

    problem 6 add

--- 1-9/problem6.py
+++ 1-9/problem6.py
@@ -1,5 +1,16 @@
 
+import math
 
+sumOfSquares = 0
+summen = 0
+
+for i in range(1, 101):
+    summen += i
+    sumOfSquares += math.pow(i, 2)
+
+summen = math.pow(summen, 2)
+
+print(summen - sumOfSquares)
 
 wait = input("PRESS ENTER TO CLOSE")
 exit()
