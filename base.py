f667146251f14a06f799546f0a66e5e84c4204a3
T0wn <33673748+T0wn@users.noreply.github.com>
2018-05-26 17:52:18 +0200

    added base file

---
+++ base.py
@@ -0,0 +1,21 @@
+
+import time
+import os
+
+print("Working...")
+start = time.time()
+
+
+
+
+
+os.system("cls")
+
+
+
+stop = time.time()
+print("Cumputing time: " + str(stop-start) + " seconds")
+
+print("")
+wait = input("PRESS ENTER TO CLOSE")
+exit()
