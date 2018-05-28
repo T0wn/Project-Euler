dfa62c985039b58e2c315d891abfa3ab67a53bd0
T0wn <33673748+T0wn@users.noreply.github.com>
2018-05-28 15:00:16 +0200

    unfinished version of problem 14

--- 10-20/problem14.py
+++ 10-20/problem14.py
@@ -1,6 +1,9 @@
 
+import time
+import os
+
 def evenNumber(number):
-    return number / 2
+    return int(number / 2)
 
 def oddNumber(number):
     return (3 * number) + 1
@@ -8,4 +11,34 @@ def oddNumber(number):
 def nextNumber(number):
     if (number % 2 == 0):
         return evenNumber(number)
-    else return oddNumber(number)
+    else:
+        return oddNumber(number)
+
+print("Working...")
+start = time.time()
+
+
+highestRekkeLen = []
+for i in range(1, 1000001):
+    print(i)
+    rekke = [i]
+    if (i in highestRekkeLen):
+        print("test")
+        continue
+    while(rekke[len(rekke) - 1] != 1):
+        rekke.append(nextNumber(rekke[len(rekke) - 1]))
+    if(len(rekke) > len(highestRekkeLen)):
+        highestRekkeLen = rekke
+
+
+
+os.system("cls")
+
+print(highestLoopCount)
+
+stop = time.time()
+print("Cumputing time: " + str(stop-start) + " seconds")
+
+print("")
+wait = input("PRESS ENTER TO CLOSE")
+exit()
